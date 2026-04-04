5d449844e8593914b385829d17887c8085af2f4b
Moin Bagban <moinbagban81@gmail.com>
2026-04-04 09:54:19 +0530

    added image field for profile_photo

--- apps/users/models.py
+++ apps/users/models.py
@@ -21,6 +21,7 @@ class User(AbstractUser):
 
     username = None
     email = models.EmailField(unique=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
     auth_provider = models.CharField(max_length=20, choices=AUTH_PROVIDERS, default=AUTH_PROVIDER_EMAIL)
     google_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
     mobile_no = models.CharField(max_length=15, unique=True, null=True, blank=True)
